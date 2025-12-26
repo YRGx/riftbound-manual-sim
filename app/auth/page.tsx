f04a25df7faafc0dc97f63b1fd55850af86957fc
YRGx <yourrandomgamer1@gmail.com>
2025-12-26 18:31:48 -0500

    Update page.tsx

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "../../src/lib/supabaseClient";
 
 export default function AuthPage() {
   const [email, setEmail] = useState("");
